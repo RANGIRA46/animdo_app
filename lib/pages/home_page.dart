389e633f129d72509c799f9ee65af82aacfd02bc
Nobody <johnsonbaraka46@gmail.com>
2025-05-15 15:49:52 +0200

    updated the plugins and created animated widget with a circle that shrinks

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,39 +1,57 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
-
   @override
-  State<StatefulWidget> createState(){
+  State<StatefulWidget> createState() {
     return _HomePageState();
   }
 }
 
 class _HomePageState extends State<HomePage> {
+  double _buttonRadius = 100;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Animdo'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '0',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: Container(
+        child: Stack(
+          clipBehavior: Clip.none,
+          children: [_pageBackground(), _centeredCircularAnimationButton()],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+    );
+  }
+
+  Widget _pageBackground() {
+    return Container(color: Colors.blue);
+  }
+
+  Widget _centeredCircularAnimationButton() {
+    return Center(
+      child: GestureDetector(
+        onTap: () {
+          setState(() {
+            // Toggle between increasing and decreasing the radius
+            _buttonRadius += _buttonRadius == 200 ? -100 : 100;
+          });
+        },
+        child: AnimatedContainer(
+          duration: const Duration(seconds: 2),
+          curve: Curves.bounceInOut,
+          width: _buttonRadius,
+          height: _buttonRadius, // Ensure it's a perfect circle
+          decoration: BoxDecoration(
+            color: Colors.purpleAccent,
+            borderRadius: BorderRadius.circular(_buttonRadius),
+          ),
+          child: const Center(
+            child: Text(
+              'Basic',
+              style: TextStyle(color: Colors.white),
+            ),
+        ),
       ),
+    ),
     );
   }
 }
