d044329fcd46b93dadbd8be1b1ac055057ea38f2
Nobody <johnsonbaraka46@gmail.com>
2025-05-16 08:10:44 +0200

    created the rotating star icon by using animation controllers

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -7,37 +9,54 @@ class HomePage extends StatefulWidget {
   }
 }
 
-class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin {
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
   double _buttonRadius = 100;
 
-  final Tween<double> _backgroundScale = Tween<double>(begin: 0.5, end: 1.2); // Scale from 0.5 to 1.2
+  final Tween<double> _backgroundScale = Tween<double>(
+    begin: 0.5,
+    end: 1.2,
+  ); // Scale from 0.5 to 1.2
   late AnimationController _controller; // Animation controller
-  late Animation<double> _backgroundAnimation; // Animation for the background scale
+  late Animation<double>
+      _backgroundAnimation; // Animation for the background scale
+  late AnimationController
+      _starIconAnimationController; // Animation controller for star icon
 
   @override
   void initState() {
     super.initState();
 
-    // Initialize the animation controller
+    // Initialize the animation controller for background
     _controller = AnimationController(
       duration: const Duration(seconds: 2),
       vsync: this,
     );
 
     // Define the animation using the Tween and the controller, with a curve
-    _backgroundAnimation = _backgroundScale.animate(CurvedAnimation(
-      parent: _controller,
-      curve: Curves.easeInOut, // Smooth easing curve for the animation
-    ));
+    _backgroundAnimation = _backgroundScale.animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Curves.easeInOut, // Smooth easing curve for the animation
+      ),
+    );
 
-    // Start the animation
+    // Start the background animation
     _controller.forward();
+
+    // Initialize the animation controller for the star icon
+    _starIconAnimationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 4),
+    );
+
+    _starIconAnimationController.repeat();
   }
 
   @override
   void dispose() {
-    // Dispose the animation controller
+    // Dispose the animation controllers
     _controller.dispose();
+    _starIconAnimationController.dispose();
     super.dispose();
   }
 
@@ -51,7 +70,15 @@ class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin
             scale: _backgroundAnimation.value, // Apply the scale animation
             child: Stack(
               clipBehavior: Clip.none,
-              children: [_pageBackground(), _centeredCircularAnimationButton()],
+              children: [
+                _pageBackground(),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.max,
+                  children: [_centeredCircularAnimationButton(), _starIcon()],
+                ),
+              ],
             ),
           );
         },
@@ -86,12 +113,26 @@ class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin
             borderRadius: BorderRadius.circular(_buttonRadius),
           ),
           child: const Center(
-            child: Text(
-              'Basic',
-              style: TextStyle(color: Colors.white),
-            ),
+            child: Text('Basic', style: TextStyle(color: Colors.white)),
           ),
         ),
+      ),
+    );
+  }
+
+  Widget _starIcon() {
+    return AnimatedBuilder(
+      animation: _starIconAnimationController.view,
+      builder: (_buildContext, _child) {
+        return Transform.rotate(
+          angle: _starIconAnimationController.value * 2 * pi, // Rotate the star icon
+          child: _child,
+        );
+      },
+      child: const Icon(
+        Icons.star,
+        size: 100,
+        color: Colors.white,
       ),
     );
   }
